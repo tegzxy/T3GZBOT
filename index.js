8921db4e6ddd8e5e05959c5e04997be84f5d4c17
tegzxy <75916532+tegzxy@users.noreply.github.com>
2020-12-13 16:55:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,14 +12,13 @@ const menu = require("./lib/menu.js");
 const donate = require("./lib/donate.js");
 const info = require("./lib/info.js");
 //
-const BotName = 'BOT ./MrG3P5'; // Nama Bot Whatsapp
-const instagramlu = 'https://www.instagram.com/mrg3p5_id/'; // Nama Instagramlu cok
-const whatsapplu = '0895-2325-8649'; // Nomor whatsapplu cok
+const BotName = 'T3GZBOT; // Nama Bot Whatsapp
+const instagramlu = 'https://www.instagram.com/tegarxzy/'; // Nama Instagramlu cok
+const whatsapplu = '0895-0429-4960'; // Nomor whatsapplu cok
 const kapanbotaktif = '24 Jam'; // Kapan bot lu aktif
-const grupch1 = 'https://chat.whatsapp.com/L0jifxElwo6B2QyxuabLtU'; // OFFICIAL GRUP LU 1
-const grupch2 = 'https://chat.whatsapp.com/CxrUD2MnUv5KGko3CsW6DT'; // OFFICIAL GRUP LU 2
+
 //
-const
+const 
 {
    WAConnection,
    MessageType,
